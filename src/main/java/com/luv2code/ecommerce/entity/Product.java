effd61997117491397589a35f625bc17c4dc2172
Rume7 <electengrhume@gmail.com>
2021-11-13 16:09:03 +0100

    Created product category and updated the product entity

--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -2,13 +2,10 @@ package com.luv2code.ecommerce.entity;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 /**
  *
@@ -21,17 +18,40 @@ public class Product {
     
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     private Long id;
     
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+    
+    @Column(name = "sku")
     private String sku;
+    
+    @Column(name = "name")
     private String name;
+    
+    @Column(name = "description")
     private String description;
+    
+    @Column(name = "unit_price")
     private BigDecimal unitPrice;
+    
+    @Column(name = "image_url")
     private String imageUrl;
+    
+    @Column(name = "active")
     private boolean active;
-    private int unitIntStock;
+    
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+    
+    @Column(name = "date_created")
+    @CreationTimestamp
     private Date dateCreated;
+    
+    @Column(name = "last_updated")
+    @UpdateTimestamp
     private Date lastUpdated;
     
     
